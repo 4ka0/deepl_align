7d407ae1e2fe49a08570f9ce3e945c3573b9ee5b
Jon Lee <39420293+4ka0@users.noreply.github.com>
2022-08-11 14:54:22 +0900

    Code cleanup

--- translate.py
+++ translate.py
@@ -127,6 +127,7 @@ if __name__ == "__main__":
         translator = setup_deepl_translator()
         source_segments = get_source_segments(source_file)
         source_char_count = get_source_char_count(source_segments)
+
         if check_deepl_usage(source_char_count, translator):
             full_segments = translate_segments(translator, source_segments, glossary_file)
 
