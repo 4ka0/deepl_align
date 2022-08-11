b324c6c21f5295f0755f05e7d32f1e0303a9349f
Jon Lee <39420293+4ka0@users.noreply.github.com>
2022-08-11 14:12:02 +0900

    Test update

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -2,10 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import os
-import pytest
 
 from .. import translate
 from .. translate import Segment
+
+import pytest
+import deepl
+from environs import Env
+
+
+# Need to use absolute paths in some of the below tests. Specifically,
+# those tests using Document and Env. Therefore need the full path to this
+# 'unit_test.py' file to build those absolute paths.
+BASE_DIR = current_dir = os.path.dirname(os.path.realpath(__file__))
 
 
 @pytest.mark.parametrize(
@@ -39,6 +48,14 @@ def segment_object():
     return segment
 
 
+@pytest.fixture
+def segment_objects_from_file():
+    test_file_path = "docs/small-PCT-JP.docx"
+    full_file_path = BASE_DIR + "/" + test_file_path
+    segments = translate.get_source_segments(full_file_path)
+    return segments
+
+
 def test_segment_constructor(segment_object):
     assert isinstance(segment_object, Segment)
 
@@ -50,16 +67,22 @@ def test_segment_object_source_text(segment_object):
 def test_segment_object_target_text(segment_object):
     assert segment_object.target_text == "The positive hole transport layers 12 may include an inorganic material."
 
-"""
-def test_get_source_segments():
 
-    # Need to give Document the absolute path to the test docx file.
-    # Therefore need the full path to this 'unit_test.py' file.
-    # Then need to add the location of the test docx file to the path.
-    current_dir = os.path.dirname(os.path.realpath(__file__))
-    test_file_path = "source_docs/small-PCT-JP.docx"
-    full_file_path = current_dir + "/" + test_file_path
+def test_setup_deepl_translator_constructor():
+    output = translate.setup_deepl_translator()
+    assert isinstance(output, deepl.Translator)
 
+
+def test_setup_deepl_auth_key_retrieval_from_env_file():
+    test_file_path = "docs/.env.test"
+    full_file_path = BASE_DIR + "/" + test_file_path
+    expected = "lskdjf84u9f8oroifjoidjfo8u8eu98ufoidjiof8u984u"
+    env = Env()
+    env.read_env(full_file_path, recurse=False)
+    assert env("KEY") == expected
+
+
+def test_get_source_segments(segment_objects_from_file):
     expected = [
         "明細書",
         "発明の名称 : 情報処理装置、情報処理方法及びプログラム",
@@ -68,7 +91,8 @@ def test_get_source_segments():
         "本技術は、自己位置を推定する情報処理装置等の技術に関する。",
         "背景技術",
         "[0002]",
-        "近年、視覚を通じて仮想空間に存在するように知覚させるＶＲ（Virtual Reality）技術や、現実世界における実物体に仮想オブジェクトを重畳表示するＡＲ（Augmented Reality）技術が広く知られるようになってきている。",
+        ("近年、視覚を通じて仮想空間に存在するように知覚させるＶＲ（Virtual Reality）技術や、現実世界における"
+         "実物体に仮想オブジェクトを重畳表示するＡＲ（Augmented Reality）技術が広く知られるようになってきている。"),
         "ＶＲ技術、ＡＲ技術は、例えば、ヘッドマウントディスプレイや、スマートフォン等の各種の情報処理装置に搭載されている。",
         "[0003]",
         "ＡＲ技術や、ＶＲ技術においては、情報処理装置における自己位置を正確に推定する必要がある（例えば、下記特許文献１参照）。",
@@ -86,7 +110,11 @@ def test_get_source_segments():
         "なお、表示部３は、非シースルータイプの表示部であってもよい。",
         "この場合、撮像部４により撮像された画像が表示部３上に表示される。",
     ]
-    output = translate.get_source_segments(full_file_path)
-
+    output = [seg_obj.source_text for seg_obj in segment_objects_from_file]
     assert output == expected
-"""
+
+
+def test_get_source_char_count(segment_objects_from_file):
+    output = translate.get_source_char_count(segment_objects_from_file)
+    expected = 686
+    assert output == expected
