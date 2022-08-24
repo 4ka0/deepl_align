55deb0cd12d8341be18b678d793126447f71862b
Jon Lee <39420293+4ka0@users.noreply.github.com>
2022-08-24 20:37:14 +0900

    Test update

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -26,19 +26,15 @@ class MockDeeplGlossary():
         self.target_lang = target_lang
         self.entries = entries
 
-    @property
     def name(self):
         return self.glossary_name
 
-    @property
     def source_lang(self):
         return self.source_lang
 
-    @property
     def target_lang(self):
         return self.target_lang
 
-    @property
     def entry_count(self):
         return self.entries
 
@@ -178,10 +174,14 @@ def test_get_source_segments(list_of_segment_objects_from_file):
         "本技術は、自己位置を推定する情報処理装置等の技術に関する。",
         "背景技術",
         "[0002]",
-        "近年、視覚を通じて仮想空間に存在するように知覚させるＶＲ（Virtual Reality）技術や、現実世界における実物体に仮想オブジェクトを重畳表示するＡＲ（Augmented Reality）技術が広く知られるようになってきている。",
-        "ＶＲ技術、ＡＲ技術は、例えば、ヘッドマウントディスプレイや、スマートフォン等の各種の情報処理装置に搭載されている。",
+        "近年、視覚を通じて仮想空間に存在するように知覚させるＶＲ（Virtual Reality）"
+        "技術や、現実世界における実物体に仮想オブジェクトを重畳表示するＡＲ"
+        "（Augmented Reality）技術が広く知られるようになってきている。",
+        "ＶＲ技術、ＡＲ技術は、例えば、ヘッドマウントディスプレイや、スマートフォン等の"
+        "各種の情報処理装置に搭載されている。",
         "[0003]",
-        "ＡＲ技術や、ＶＲ技術においては、情報処理装置における自己位置を正確に推定する必要がある（例えば、下記特許文献１参照）。",
+        "ＡＲ技術や、ＶＲ技術においては、情報処理装置における自己位置を正確に推定する"
+        "必要がある（例えば、下記特許文献１参照）。",
         "先行技術文献",
         "特許文献",
         "[0004]",
@@ -189,10 +189,14 @@ def test_get_source_segments(list_of_segment_objects_from_file):
         "発明の概要",
         "発明が解決しようとする課題",
         "[0005]",
-        "しかしながら、情報処理装置（ヘッドマウントディスプレイ等）を付帯するユーザが、車、電車等の乗り物に乗っているような特定の状況下にある場合、自己位置の推定の精度が低下してしまうといった問題がある。",
+        "しかしながら、情報処理装置（ヘッドマウントディスプレイ等）を付帯するユーザが、"
+        "車、電車等の乗り物に乗っているような特定の状況下にある場合、自己位置の推定"
+        "の精度が低下してしまうといった問題がある。",
         "[0034]",
         "表示部３は、シースルータイプの表示部であり、グラス部１５の表面に設けられている。",
-        "表示部３は、制御部１の制御に応じて、仮想オブジェクト２０（図１０参照）を表示することで、グラス部１５を介してユーザが見ている実空間上に仮想オブジェクト２０が配置されているようにユーザに認識させる。",
+        "表示部３は、制御部１の制御に応じて、仮想オブジェクト２０（図１０参照）を表示する"
+        "ことで、グラス部１５を介してユーザが見ている実空間上に仮想オブジェクト２０が"
+        "配置されているようにユーザに認識させる。",
         "なお、表示部３は、非シースルータイプの表示部であってもよい。",
         "この場合、撮像部４により撮像された画像が表示部３上に表示される。",
     ]
@@ -216,12 +220,6 @@ def test_check_deepl_usage_fail(mock_deepl_translator):
     source_char_count = 200000
     output = translate.check_deepl_usage(source_char_count, mock_deepl_translator)
     assert output is False
-
-
-def test_translate_segments(mock_deepl_translator, list_of_segment_objects):
-    segments = translate.translate_segments(mock_deepl_translator, list_of_segment_objects, None)
-    for segment in segments:
-        assert segment.target_text == "mock_target_string"
 
 
 def test_extract_glossary_entries_success():
@@ -270,7 +268,30 @@ def test_extract_glossary_entries_fail_3():
         translate.extract_glossary_entries(full_file_path)
 
 
-def test_create_deepl_glossary_success():
+def test_create_deepl_glossary_success(mock_deepl_translator, mock_glossary_entries):
+    glossary_name = "Test glossary"
+    output = translate.create_deepl_glossary(
+        mock_deepl_translator,
+        glossary_name,
+        mock_glossary_entries
+    )
+    assert output.glossary_name == "Test glossary"
+    assert output.source_lang == "JA"
+    assert output.target_lang == "en-US"
+    assert output.entries == mock_glossary_entries
+
+
+def test_translate_segments_without_glossary(mock_deepl_translator, list_of_segment_objects):
+    segments = translate.translate_segments(mock_deepl_translator, list_of_segment_objects, None)
+    for segment in segments:
+        assert segment.target_text == "mock_target_string"
+
+
+def test_translate_segments_with_glossary():
+    pass
+
+
+def test_create_tmx():
     pass
 
 
