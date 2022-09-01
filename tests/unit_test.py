513f29f997ae8997870fdd565ac0d7d009f182e4
Jon Lee <39420293+4ka0@users.noreply.github.com>
2022-09-01 21:09:22 +0900

    Test update

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -520,7 +520,7 @@ def test_create_docx_file_exists(list_of_translated_segment_objects):
     assert os.path.exists(docx_file_path) is False
 
 
-def test_create_docx_file_table_structure(list_of_translated_segment_objects):
+def test_create_docx_file_check_table_structure(list_of_translated_segment_objects):
     docx_name = "test-docx-file"
     translate.create_docx(docx_name, list_of_translated_segment_objects)
     document = Document("output/test-docx-file.docx")
@@ -530,5 +530,26 @@ def test_create_docx_file_table_structure(list_of_translated_segment_objects):
     assert len(document.tables[0].rows) == 10
 
 
-def test_create_docx_file_table_content(list_of_translated_segment_objects):
-    pass
+def test_create_docx_file_check_table_content(list_of_translated_segment_objects):
+    # create document
+    docx_name = "test-docx-file"
+    translate.create_docx(docx_name, list_of_translated_segment_objects)
+
+    # read in created document from table in docx file
+    parent_dir = os.path.join(BASE_DIR, os.pardir)
+    docx_file_path = os.path.join(parent_dir, "output/test-docx-file.docx")
+
+    document = Document(docx_file_path)
+    table = document.tables[0]
+
+    for row in table.rows:
+        assert row.cells[0].text == "正孔輸送層12"
+        assert row.cells[1].text == "positive hole transport layers 12"
+
+    # Clean up after test
+    try:
+        os.remove(docx_file_path)
+    except OSError:
+        pass
+
+    assert os.path.exists(docx_file_path) is False
